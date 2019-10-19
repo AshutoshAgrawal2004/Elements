70fa750cc94518a2562e6f1d9ecb878fbfc51ddf
AshutoshAgrawal2004 <agrawalnidhi635@gmail.com>
2019-10-19 21:14:33 +0530

    Updates

---
+++ precache-manifest.1fca283a8891cd86842503687296d45c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b7a6fc20e0ab3c539b46acc8c2aa71",
+    "url": "/Elements/index.html"
+  },
+  {
+    "revision": "62843d1467fba83674bd",
+    "url": "/Elements/static/css/2.8bed955a.chunk.css"
+  },
+  {
+    "revision": "ba876da4ddd71132ddfa",
+    "url": "/Elements/static/css/main.a90a8812.chunk.css"
+  },
+  {
+    "revision": "62843d1467fba83674bd",
+    "url": "/Elements/static/js/2.4ef68f36.chunk.js"
+  },
+  {
+    "revision": "ba876da4ddd71132ddfa",
+    "url": "/Elements/static/js/main.462fca44.chunk.js"
+  },
+  {
+    "revision": "be3727a8f46c2b252e43",
+    "url": "/Elements/static/js/runtime-main.480db17f.js"
+  }
+]);

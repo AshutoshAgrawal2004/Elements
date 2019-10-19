623d00f2f48aaae08c357568168a7ed548a89737
AshutoshAgrawal2004 <agrawalnidhi635@gmail.com>
2019-10-19 20:48:38 +0530

    About Page

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import store from './store';
 import ElementDetail from './components/elements/ElementDetail';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
-
 import Home from './components/pages/Home';
+import About from './components/pages/About';
 
 function App() {
 	useEffect(() => {
@@ -22,7 +22,8 @@ function App() {
 				<Header />
 
 				<Switch>
-					<Route exact path='/' component={Home} />
+					<Route exact path='/' component={Home} />{' '}
+					<Route exact path='/about' component={About} />
 					<Route
 						exact
 						path='/elements/:name'

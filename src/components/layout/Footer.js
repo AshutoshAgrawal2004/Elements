c49e11d5e110a960359a80f2851fb340c1b15734
AshutoshAgrawal2004 <agrawalnidhi635@gmail.com>
2019-10-19 20:08:17 +0530

    Footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,44 +2,25 @@ import React from 'react';
 
 const Footer = () => {
 	return (
-		<footer class='page-footer'>
-			<div class='container'>
-				<div class='row'>
-					<div class='col l6 s12'>
-						<h5>Made with &hearts; by <a href="https://www.sololearn.com/Profile/10779261">Ashutosh Agrawal</a></h5>
-					</div>
-					<div class='col l4 offset-l2 s12'>
-						<h5 class='white-text'>Links</h5>
-						<ul>
-							<li>
-								<a class='grey-text text-lighten-3' href='#!'>
-									Link 1
-								</a>
-							</li>
-							<li>
-								<a class='grey-text text-lighten-3' href='#!'>
-									Link 2
-								</a>
-							</li>
-							<li>
-								<a class='grey-text text-lighten-3' href='#!'>
-									Link 3
-								</a>
-							</li>
-							<li>
-								<a class='grey-text text-lighten-3' href='#!'>
-									Link 4
-								</a>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			<div class='footer-copyright'>
-				<div class='container'>
+		<footer className='page-footer center'>
+			<h6>
+				Made with &hearts; by{' '}
+				<a
+					className='grey-text text-lighten-4'
+					href='https://www.sololearn.com/Profile/10779261'
+				>
+					Ashutosh Agrawal
+				</a>
+			</h6>
+
+			<div className='footer-copyright'>
+				<div className='container'>
 					© 2019 Elemental
-					<a class='grey-text text-lighten-4 right' href='https://github.com/AshutoshAgrawal2004'>
-            Git Repo
+					<a
+						class='grey-text text-lighten-4 right'
+						href='https://github.com/AshutoshAgrawal2004'
+					>
+						Git Repo
 					</a>
 				</div>
 			</div>
